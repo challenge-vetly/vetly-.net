621d382cee6730042ea7e28ce59560bc6defdb17
Tiagozguedes <tiagozguedes@gmail.com>
2026-05-12 02:35:20 -0300

    feat(domain): adiciona entidade Empresa com administrador e tipo

---
+++ src/Vetly.Domain/Entities/Empresa.cs
@@ -0,0 +1,31 @@
+namespace Vetly.Domain.Entities;
+
+public class Empresa
+{
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; }
+    public string Tipo { get; private set; }
+    public Guid AdministradorId { get; private set; }
+    public bool Ativa { get; private set; }
+
+    private Empresa() { }
+
+    public Empresa(string nome, string tipo, Guid administradorId)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Tipo = tipo;
+        AdministradorId = administradorId;
+        Ativa = true;
+    }
+
+    public void AtualizarDados(string nome, string tipo)
+    {
+        Nome = nome;
+        Tipo = tipo;
+    }
+
+    public void AlterarAdministrador(Guid novoAdminId) => AdministradorId = novoAdminId;
+
+    public void Desativar() => Ativa = false;
+}
